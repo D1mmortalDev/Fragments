3ab94fc8dbc34b5aa0b5ab9a963adcc3c07d09a8
Bernadette <bernadettedejesus99@gmail.com>
2023-07-15 18:13:55 +0800

    first commit

---
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fragments.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val firstFragment = FirstFragment()
+        val secondFragment =SecondFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,firstFragment)
+            commit()
+        }
+        binding.btnFragment1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment,firstFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+        binding.btnFragment2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment,secondFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+    }
+}
